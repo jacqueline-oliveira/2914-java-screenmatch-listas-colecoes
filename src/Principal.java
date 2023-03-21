b4e85fb7beb8e067ea10bea63fda1d2d6fd03a14
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-21 12:39:30 -0300

    vídeo 1.3

--- src/Principal.java
+++ src/Principal.java
@@ -66,6 +66,7 @@ public class Principal {
         System.out.println("Tamanho da lista " + listaDeFilmes.size());
         System.out.println("Primeiro filme " + listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes);
+        System.out.println("toString do filme " + listaDeFilmes.get(0).toString());
 
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -17,4 +17,9 @@ public class Filme extends Titulo implements Classificavel {
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
     }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getDuracaoEmMinutos() + ")";
+    }
 }
